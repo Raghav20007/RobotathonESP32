daf7f1d168c91be4fbb227993bd6a47dfb9b3b59
Excalibur !! <docknockocean@gmail.com>
2025-11-08 15:05:08 -0600

    new color code

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -1,65 +1,139 @@
-
-#include "sdkconfig.h"
 #include <Arduino.h>
+#include <Wire.h>
+#include <Arduino_APDS9960.h>
 #include <Bluepad32.h>
-#include <uni.h>
 #include "controller_callbacks.h"
 
-#define IN1  16  // Control pin 1
-#define IN2  17  // Control pin 2
-#define IN3  18  // Control pin 3
-#define IN4  19  // Control pin 4
 
-extern ControllerPtr myControllers[BP32_MAX_GAMEPADS]; // BP32 library allows for up to 4 concurrent controller connections, but we only need 1
+#define APDS9960_INT 2
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define I2C_FREQ 100000
 
-// Controller code
-void foo(ControllerPtr myController) {
-  while(1) {
-    BP32.update();
-    if(myController->axisY()) {
-      // Spin motor
-      digitalWrite(IN1, 1);  // PWM signal
-      digitalWrite(IN2, LOW); // Direction control
-      digitalWrite(IN3, LOW);  // PWM signal
-      digitalWrite(IN4, 1); // Direction control
-      return;
+APDS9960 sensor = APDS9960(Wire, APDS9960_INT);
+
+
+#define IN1 16
+#define IN2 17
+#define IN3 18
+#define IN4 19
+
+
+extern ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+int colorCollect[4];   // saved color
+bool isCollected = false;
+bool colorFound = false;
+
+
+int r, g, b, a;
+
+void setupColor() {
+    Wire.begin(I2C_SDA, I2C_SCL, I2C_FREQ);
+    sensor.setInterruptPin(APDS9960_INT);
+    if (!sensor.begin()) {
+        Serial.println("APDS9960 error");
+        while (1);
     }
-    else {
-      // Stop motor
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, LOW);
-    }
-  }
+    Serial.begin(115200);
 }
+
+void ActivationC() {
+    while (!sensor.colorAvailable()) {
+        vTaskDelay(1);
+    }
+    sensor.readColor(r, g, b, a);
+    colorCollect[0] = r;
+    colorCollect[1] = g;
+    colorCollect[2] = b;
+    colorCollect[3] = a;
+    isCollected = true;
+    Serial.printf("Saved color: R=%d G=%d B=%d A=%d\n", r, g, b, a);
+}
+
+
+void moveForward() {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+void stopMotors() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+}
+
+
+void foo(ControllerPtr myController) {
+    BP32.update();
+
+  
+    if (myController->b() && !isCollected) {
+        ActivationC();
+        return;
+    }
+
+   
+    if (myController->y() && isCollected) {
+        Serial.println("Moving");
+        colorFound = false;
+        moveForward();
+
+        while (!colorFound) {
+            if (!sensor.colorAvailable()) {
+                vTaskDelay(1);
+                continue;
+            }
+
+            sensor.readColor(r, g, b, a);
+            Serial.printf("R:%d G:%d B:%d\n", r, g, b);
+
+            int tolerance = 15;
+            bool match =
+                abs(r - colorCollect[0]) < tolerance &&
+                abs(g - colorCollect[1]) < tolerance &&
+                abs(b - colorCollect[2]) < tolerance;
+
+            if (match) {
+                colorFound = true;
+                stopMotors();
+                Serial.println("Target color detected");
+                isCollected = false;
+            }
+            vTaskDelay(10);
+        }
+    }
+}
+
 
 void setup() {
-  // Motor Setup
-  Serial.begin(115200);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+    Serial.begin(115200);
 
-  // Controller Setup
-  BP32.setup(&onConnectedController, &onDisconnectedController);
-  BP32.forgetBluetoothKeys(); 
-  esp_log_level_set("gpio", ESP_LOG_ERROR); // Suppress info log spam from gpio_isr_service
-  uni_bt_allowlist_set_enabled(true);
+  
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
+  
+    setupColor();
+
+   
+    BP32.setup(&onConnectedController, &onDisconnectedController);
+    BP32.forgetBluetoothKeys();
+    esp_log_level_set("gpio", ESP_LOG_ERROR); // suppress extra logs
 }
 
+
 void loop() {
+    vTaskDelay(1); 
+    BP32.update();
 
-    // Controller Loop Code
-    vTaskDelay(1); // Ensures WDT does not get triggered when no controller is connected
-    BP32.update(); 
-
-    for (auto myController : myControllers) { // Only execute code when controller is connected
-        if (myController && myController->isConnected() && myController->hasData()) {        
-            
+    for (auto myController : myControllers) {
+        if (myController && myController->isConnected() && myController->hasData()) {
             foo(myController);
-
         }
     }
 }
